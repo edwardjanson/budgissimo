3a66778efb8774a4292c94c76bcab895fb33326e
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-11-11 15:33:50 +0000

    🔨 added the currency class

---
+++ models/currency.py
@@ -0,0 +1,7 @@
+class Currency:
+
+    def __init__(self, name, symbol, symbol_location, id=None):
+        self.name = name
+        self.symbol = symbol
+        self.symbol_location = symbol_location
+        self.id = id
