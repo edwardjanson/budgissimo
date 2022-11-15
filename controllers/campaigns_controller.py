4f6aa3dbb59ba5837af3c9881134f4bcd0722c22
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-11-15 15:05:29 +0000

    added the campaigns edit.html

--- controllers/campaigns_controller.py
+++ controllers/campaigns_controller.py
@@ -21,7 +21,7 @@ def new_campaign(platform_id):
         platform = platform_repository.select(platform_id)
         categories = tag_repository.get_tag_categories_and_names(account)
 
-        return render_template("/platforms/campaigns/new.html", platform=platform, categories=categories)
+        return render_template("platforms/campaigns/new.html", platform=platform, categories=categories)
 
     else: 
         return render_template("access_denied.html")
@@ -68,12 +68,47 @@ def tag_details(platform_id, campaign_id):
         return render_template("access_denied.html")
 
 
-@campaigns_blueprint.route("/campaigns/<platform_id>/campaigns/<campaign_id>/edit")
-def edit_campaign(platform_id, campaign_id):
+@campaigns_blueprint.route("/platforms/<platform_id>/campaigns/edit")
+def edit_all_campaigns(platform_id):
     if account_repository.request_allowed(session["account_id"], "platform_id", platform_id):
-        campaign = campaign_repository.select(campaign_id)
-        return render_template('campaigns/edit.html', campaign=campaign)
+        account = account_repository.select(session["account_id"])
+        platform = platform_repository.select(platform_id)
+        campaigns = campaign_repository.select_all_by_platform(platform)
 
+        return render_template("platforms/campaigns/edit.html", account=account, platform=platform, campaigns=campaigns)
+
+    else: 
+        return render_template("access_denied.html")
+
+
+@campaigns_blueprint.route("/platforms/<platform_id>/campaigns/edit", methods=["POST"])
+def edit_campaign(platform_id):
+    if account_repository.request_allowed(session["account_id"], "platform_id", platform_id):
+        platform = platform_repository.select(platform_id)
+        campaigns = campaign_repository.select_all_by_platform(platform)
+
+        if request.form["action"] == "Apply Changes":
+            for campaign in campaigns:
+                campaign_name = request.form[f"campaign_name_{campaign.id}"]
+                monthly_budget = float(request.form[f"monthly_budget_{campaign.id}"])
+                amount_spent = float(request.form[f"amount_spent_{campaign.id}"]) if request.form[f"amount_spent_{campaign.id}"] else None
+
+                campaign = campaign_repository.select(campaign.id)
+                updated_budget = Budget(monthly_budget, amount_spent, campaign.budget.id) # type: ignore
+                budget_repository.update(updated_budget)
+
+                updated_campaign = Campaign(campaign_name, updated_budget, campaign.platform, campaign.id) # type: ignore
+                        
+                campaign_repository.update(updated_campaign)
+        
+        elif request.form["action"] == "Delete Selected":
+            for campaign in campaigns:
+                if request.form.get(f"campaign_{campaign.id}"):
+                    campaign_repository.delete(campaign.id)
+        
+        return redirect(f"/platforms/{platform_id}")
+        
+    
     else: 
         return render_template("access_denied.html")
 
